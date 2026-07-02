76903cc2fe294260dfdad7578bd0f2c7781640f3
coffievan10-source <coffievan10@gmail.com>
2026-07-02 01:07:58 +0000

    fix: specify collection name

--- models/Product.js
+++ models/Product.js
@@ -10,4 +10,4 @@ const productSchema = new mongoose.Schema({
     drop: String
 })
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model('Product', productSchema, 'products')
